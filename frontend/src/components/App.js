d50d91a0ac8f5663c4a2900a8cf350b3ef6abc2b
Dmitry Karpenko <Demonkainzzzz@mail.ru>
2023-07-01 00:52:41 +0300

    изменил порядок карточек

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -44,7 +44,7 @@ function App() {
       Promise.all([api.getUserInfo(), api.getAllCards()])
         .then(([user, cards]) => {
           setCurrentUser(user);
-          setCards(cards);
+          setCards(cards.reverse());
         })
         .catch((err) => {
           console.log(err);
